dcb88dd65963a75cd9590132a804f56583ddc0ba
yunus <geldabaev@mail.ru>
2024-04-27 08:19:20 +0300

    test

---
+++ blog/models.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+
+class MyPublish(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    address = models.CharField(max_length=79)
+    phone_number = models.CharField(max_length=9)
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+
+    def __str__(self):
+        return self.title
+
