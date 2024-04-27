0a911ad723b112efedab5c49028ec95a6252e653
yunus <geldabaev@mail.ru>
2024-04-27 10:07:39 +0300

    test2

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import MyPublish
+from django.utils import timezone
 
-# Create your views here.
+
+def index(request):
+    posts = MyPublish.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/index.html', {'posts': posts})
+
+
+
+def categ(request, catid):
+    return HttpResponse(f"<h1>Cats{catid}</h1>")
+
+
