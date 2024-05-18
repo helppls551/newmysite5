cf758c04cbde08aae896efa35ab7e7f820a7548f
Ansar <geldabaev@mail.ru>
2024-05-18 11:31:58 +0300

    test

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('post/<int:pk>/', views.post_info, name='post_info'),
     path('post/new', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit', views.post_edit, name='post_edit'),
+
 ]
 
